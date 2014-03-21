2852afce9b1c0996f9fe5033bf5ed139f2c97ec9
Yorick Peterse <yorickpeterse@gmail.com>
2014-03-21 16:59:44 +0100

    Benchmark for measuring CDATA lexing.

---
+++ benchmark/lexer/bench_cdata.rb
@@ -0,0 +1,22 @@
+require_relative '../../lib/oga'
+require 'benchmark/ips'
+
+string = 'Hello, how are you doing today?'
+small  = "<![CDATA[#{string}]]>"
+medium = "<![CDATA[#{string * 1_000}]]>"
+large  = "<![CDATA[#{string * 10_000}]]>"
+lexer  = Oga::Lexer.new
+
+Benchmark.ips do |bench|
+  bench.report 'CDATA with a small body' do
+    lexer.lex(small)
+  end
+
+  bench.report 'CDATA with a medium body' do
+    lexer.lex(medium)
+  end
+
+  bench.report 'CDATA with a large body' do
+    lexer.lex(large)
+  end
+end
--- oga.gemspec
+++ oga.gemspec
@@ -24,4 +24,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'yard'
   s.add_development_dependency 'simplecov'
   s.add_development_dependency 'kramdown'
+  s.add_development_dependency 'benchmark-ips'
 end
