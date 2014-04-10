144c95cbb44e3528f03a1b553fe9cfe35ce646e4
Yorick Peterse <yorickpeterse@gmail.com>
2014-04-10 21:31:01 +0200

    Replaced the HRS fixture with one from Gist.
    The HRS output is invalid, which Oga can not handle at this time.

--- benchmark/lexer/bench_html_time.rb
+++ benchmark/lexer/bench_html_time.rb
@@ -1,7 +1,7 @@
 require_relative '../../lib/oga'
 require 'benchmark'
 
-html = File.read(File.expand_path('../../fixtures/hrs.html', __FILE__))
+html = File.read(File.expand_path('../../fixtures/gist.html', __FILE__))
 
 Benchmark.bmbm(20) do |bench|
   bench.report 'lex HTML' do
--- benchmark/parser/bench_html.rb
+++ benchmark/parser/bench_html.rb
@@ -1,7 +1,7 @@
 require_relative '../../lib/oga'
 require 'benchmark/ips'
 
-html = File.read(File.expand_path('../../fixtures/hrs.html', __FILE__))
+html = File.read(File.expand_path('../../fixtures/gist.html', __FILE__))
 
 Benchmark.ips do |bench|
   bench.report 'parse HTML' do
