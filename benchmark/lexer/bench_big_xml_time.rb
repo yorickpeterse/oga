e26d5a866456e6caed126d01d1b7a3c4f9ebf1e9
Yorick Peterse <yorickpeterse@gmail.com>
2014-05-01 12:25:49 +0200

    Removed unused variable in a lexer benchmark.

--- benchmark/lexer/bench_big_xml_time.rb
+++ benchmark/lexer/bench_big_xml_time.rb
@@ -1,8 +1,7 @@
 require_relative '../../lib/oga'
 require 'benchmark'
 
-xml   = File.read(File.expand_path('../../fixtures/big.xml', __FILE__))
-lexer = Oga::XML::Lexer.new(xml)
+xml = File.read(File.expand_path('../../fixtures/big.xml', __FILE__))
 
 Benchmark.bmbm(10) do |bench|
   bench.report '10MB of XML' do
