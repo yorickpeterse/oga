ad67cd708f92b4a4a8a0c6adfbf59c1c46ef3ae7
Yorick Peterse <yorickpeterse@gmail.com>
2014-05-15 20:43:48 +0200

    Only include debug info when DEBUG is set.

--- ext/c/extconf.rb
+++ ext/c/extconf.rb
@@ -5,9 +5,9 @@ have_header('ruby.h')
 $CFLAGS << ' -Wextra -Wall -pedantic'
 
 if ENV['DEBUG']
-  $CFLAGS << ' -O0'
+  $CFLAGS << ' -O0 -g'
 else
-  $CFLAGS << ' -O3 -g'
+  $CFLAGS << ' -O3'
 end
 
 create_makefile('liboga/liboga')
