4b408eae4ff775b871439d6ee3d32e68fb4ac0c8
Yorick Peterse <yorickpeterse@gmail.com>
2014-07-01 10:09:42 +0200

    Tests for Node#children=.

--- spec/oga/xml/node_spec.rb
+++ spec/oga/xml/node_spec.rb
@@ -15,4 +15,24 @@ describe Oga::XML::Node do
       described_class.new.node_type.should == :node
     end
   end
+
+  context '#children=' do
+    example 'set the child nodes using an Array' do
+      child = described_class.new
+      node  = described_class.new
+
+      node.children = [child]
+
+      node.children[0].should == child
+    end
+
+    example 'set the child nodes using a NodeSet' do
+      child = described_class.new
+      node  = described_class.new
+
+      node.children = Oga::XML::NodeSet.new([child])
+
+      node.children[0].should == child
+    end
+  end
 end
