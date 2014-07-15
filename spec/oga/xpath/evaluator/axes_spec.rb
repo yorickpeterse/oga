e0544959eeb27c08d7e14f876d9ed7093e2e59bd
Yorick Peterse <yorickpeterse@gmail.com>
2014-07-15 09:37:47 +0200

    Specs for the ancestor-or-self axis.

--- spec/oga/xpath/evaluator/axes_spec.rb
+++ spec/oga/xpath/evaluator/axes_spec.rb
@@ -30,8 +30,50 @@ describe Oga::XPath::Evaluator do
 
       it_behaves_like :node_set, :length => 1
 
-      example 'return the <b> ancestor' do
+      example 'return the <a> ancestor' do
         @set[0].name.should == 'a'
+      end
+    end
+  end
+
+  context 'ancestor-or-self axis' do
+    before do
+      @evaluator = described_class.new(@c_node)
+    end
+
+    context 'direct ancestors' do
+      before do
+        @set = @evaluator.evaluate('ancestor-or-self::b')
+      end
+
+      it_behaves_like :node_set, :length => 1
+
+      example 'return the <b> ancestor' do
+        @set[0].name.should == 'b'
+      end
+    end
+
+    context 'higher ancestors' do
+      before do
+        @set = @evaluator.evaluate('ancestor-or-self::a')
+      end
+
+      it_behaves_like :node_set, :length => 1
+
+      example 'return the <a> ancestor' do
+        @set[0].name.should == 'a'
+      end
+    end
+
+    context 'missing ancestors' do
+      before do
+        @set = @evaluator.evaluate('ancestor-or-self::c')
+      end
+
+      it_behaves_like :node_set, :length => 1
+
+      example 'return the <c> node' do
+        @set[0].name.should == 'c'
       end
     end
   end
