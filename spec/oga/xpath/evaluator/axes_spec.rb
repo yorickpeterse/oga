9a8f53bb496ccc1f6f1752c7097c03d3097b7e4e
Yorick Peterse <yorickpeterse@gmail.com>
2014-07-15 09:35:13 +0200

    Use a shared example in the axes evaluation specs.

--- spec/oga/xpath/evaluator/axes_spec.rb
+++ spec/oga/xpath/evaluator/axes_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe Oga::XPath::Evaluator do
   before do
     @document = parse('<a><b><c></c></b><d></d></a>')
+    @c_node   = @document.children[0].children[0].children[0]
   end
 
   context 'ancestor axis' do
     before do
-      c_node     = @document.children[0].children[0].children[0]
-      @evaluator = described_class.new(c_node)
+      @evaluator = described_class.new(@c_node)
     end
 
     context 'direct ancestors' do
@@ -16,13 +16,7 @@ describe Oga::XPath::Evaluator do
         @set = @evaluator.evaluate('ancestor::b')
       end
 
-      example 'return a NodeSet instance' do
-        @set.is_a?(Oga::XML::NodeSet).should == true
-      end
-
-      example 'return the right amount of rows' do
-        @set.length.should == 1
-      end
+      it_behaves_like :node_set, :length => 1
 
       example 'return the <b> ancestor' do
         @set[0].name.should == 'b'
@@ -34,13 +28,7 @@ describe Oga::XPath::Evaluator do
         @set = @evaluator.evaluate('ancestor::a')
       end
 
-      example 'return a NodeSet instance' do
-        @set.is_a?(Oga::XML::NodeSet).should == true
-      end
-
-      example 'return the right amount of rows' do
-        @set.length.should == 1
-      end
+      it_behaves_like :node_set, :length => 1
 
       example 'return the <b> ancestor' do
         @set[0].name.should == 'a'
