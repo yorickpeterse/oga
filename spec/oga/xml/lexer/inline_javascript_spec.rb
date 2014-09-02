868df603ab45505b87a601e39f66b3891f8fabe9
Yorick Peterse <yorickpeterse@gmail.com>
2014-09-03 00:26:24 +0200

    Added spec for inline JS + comments.

--- spec/oga/xml/lexer/inline_javascript_spec.rb
+++ spec/oga/xml/lexer/inline_javascript_spec.rb
@@ -3,23 +3,25 @@ require 'spec_helper'
 describe Oga::XML::Lexer do
   context 'lexing inline Javascript' do
     before do
-      @javascript = <<-EOF.strip
-(function()
-{
-  if ( some_number < 10 )
-  {
-    console.log('Hello');
-  }
-})();
-      EOF
+      @javascript = 'if ( number < 10 ) { }'
     end
 
     example 'lex inline Javascript' do
-      lex("<script>\n#{@javascript}\n</script>").should == [
+      lex("<script>#{@javascript}</script>").should == [
         [:T_ELEM_START, nil, 1],
         [:T_ELEM_NAME, 'script', 1],
-        [:T_TEXT, "\n#{@javascript}\n", 1],
-        [:T_ELEM_END, nil, 9]
+        [:T_TEXT, @javascript, 1],
+        [:T_ELEM_END, nil, 1]
+      ]
+    end
+
+    example 'lex inline Javascript containing an XML comment' do
+      lex("<script>#{@javascript}<!--foo--></script>").should == [
+        [:T_ELEM_START, nil, 1],
+        [:T_ELEM_NAME, 'script', 1],
+        [:T_TEXT, @javascript, 1],
+        [:T_ELEM_END, nil, 1],
+        [:T_COMMENT, 'foo', 1]
       ]
     end
   end
