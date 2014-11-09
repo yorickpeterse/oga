ceed3a6046783f70b76dede1058d009e91fe7ef7
Yorick Peterse <yorickpeterse@gmail.com>
2014-11-09 18:52:04 +0100

    Rewrote XPath evaluator predicates spec.

--- spec/oga/xpath/evaluator/predicates_spec.rb
+++ spec/oga/xpath/evaluator/predicates_spec.rb
@@ -3,20 +3,18 @@ require 'spec_helper'
 describe Oga::XPath::Evaluator do
   context 'predicates' do
     before do
-      @document  = parse('<root><a></a><b>10</b><b>20</b></root>')
-      @evaluator = described_class.new(@document)
+      @document = parse('<root><b>10</b><b>20</b></root>')
+
+      @b1 = @document.children[0].children[0]
+      @b2 = @document.children[0].children[1]
     end
 
-    context 'using predicate indexes' do
-      before do
-        @set = @evaluator.evaluate('root/b[2]')
-      end
+    example 'evaluate a predicate that returns the first <b> node' do
+      evaluate_xpath(@document, 'root/b[1]').should == node_set(@b1)
+    end
 
-      it_behaves_like :node_set, :length => 1
-
-      example 'return the second <b> node' do
-        @set[0].should == @document.children[0].children[-1]
-      end
+    example 'evaluate a predicate that returns the second <b> node' do
+      evaluate_xpath(@document, 'root/b[2]').should == node_set(@b2)
     end
   end
 end
