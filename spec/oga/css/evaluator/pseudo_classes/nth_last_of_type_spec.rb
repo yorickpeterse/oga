448ff56e38e67b48b8cba981d46d840464ea37ab
Yorick Peterse <yorickpeterse@gmail.com>
2014-11-15 18:27:26 +0100

    Fixed CSS eval specs for nth-(first|last)-of-type.

--- spec/oga/css/evaluator/pseudo_classes/nth_last_of_type_spec.rb
+++ spec/oga/css/evaluator/pseudo_classes/nth_last_of_type_spec.rb
@@ -3,48 +3,57 @@ require 'spec_helper'
 describe 'CSS selector evaluation' do
   context ':nth-last-of-type pseudo class' do
     before do
-      @document = parse('<root><a1 /><a2 /><a3 /><a4 /></root>')
+      @document = parse(<<-EOF.strip)
+<root>
+  <a id="1" />
+  <a id="2" />
+  <a id="3" />
+  <b>
+    <a id="4" />
+  </b>
+</root>
+      EOF
 
       @root = @document.children[0]
-      @a1   = @root.children[0]
-      @a2   = @root.children[1]
-      @a3   = @root.children[2]
-      @a4   = @root.children[3]
+      @a1   = @root.at_xpath('a[1]')
+      @a2   = @root.at_xpath('a[2]')
+      @a3   = @root.at_xpath('a[3]')
+      @a4   = @root.at_xpath('b/a')
     end
 
-    example 'return a node set containing the last node' do
-      evaluate_css(@document, 'root :nth-last-of-type(1)')
-        .should == node_set(@a4)
-    end
-
-    example 'return a node set containing even nodes' do
-      evaluate_css(@document, 'root :nth-last-of-type(even)')
-        .should == node_set(@a1, @a3)
-    end
-
-    example 'return a node set containing odd nodes' do
-      evaluate_css(@document, 'root :nth-last-of-type(odd)')
-        .should == node_set(@a2, @a4)
-    end
-
-    example 'return a node set containing every 2 nodes starting at node 3' do
-      evaluate_css(@document, 'root :nth-last-of-type(2n+2)')
-        .should == node_set(@a1, @a3)
-    end
-
-    example 'return a node set containing all nodes' do
-      evaluate_css(@document, 'root :nth-last-of-type(n)')
-        .should == @root.children
-    end
-
-    example 'return a node set containing the first two nodes' do
-      evaluate_css(@document, 'root :nth-last-of-type(-n+2)')
+    example 'return a node set containing the first child node' do
+      evaluate_css(@document, 'root a:nth-last-of-type(1)')
         .should == node_set(@a3, @a4)
     end
 
+    example 'return a node set containing even nodes' do
+      evaluate_css(@document, 'root a:nth-last-of-type(even)')
+        .should == node_set(@a2)
+    end
+
+    example 'return a node set containing odd nodes' do
+      evaluate_css(@document, 'root a:nth-last-of-type(odd)')
+        .should == node_set(@a1, @a3, @a4)
+    end
+
+    example 'return a node set containing every 2 nodes starting at node 2' do
+      evaluate_css(@document, 'root a:nth-last-of-type(2n+2)')
+        .should == node_set(@a2)
+    end
+
+    example 'return a node set containing all nodes' do
+      evaluate_css(@document, 'root a:nth-last-of-type(n)')
+        .should == node_set(@a1, @a2, @a3, @a4)
+    end
+
+    example 'return a node set containing the first two nodes' do
+      evaluate_css(@document, 'root a:nth-last-of-type(-n+2)')
+        .should == node_set(@a2, @a3, @a4)
+    end
+
     example 'return a node set containing all nodes starting at node 2' do
-      evaluate_css(@document, 'root :nth-last-of-type(n+2)')
-        .should == node_set(@a1, @a2, @a3)
+      evaluate_css(@document, 'root a:nth-last-of-type(n+2)')
+        .should == node_set(@a1, @a2)
     end
   end
 end
