194d981996ac6312a32f107469d3890b00fdbc02
Yorick Peterse <yorickpeterse@gmail.com>
2015-02-26 22:01:46 +0100

    XPath specs for paths with multiple members.

--- spec/oga/xpath/parser/paths_spec.rb
+++ spec/oga/xpath/parser/paths_spec.rb
@@ -13,11 +13,37 @@ describe Oga::XPath::Parser do
       parse_xpath('A').should == s(:axis, 'child', s(:test, nil, 'A'))
     end
 
+    it 'parses a relative path using two steps' do
+      parse_xpath('A/B').should == s(
+        :path,
+        s(:axis, 'child', s(:test, nil, 'A')),
+        s(:axis, 'child', s(:test, nil, 'B')),
+      )
+    end
+
+    it 'parses a relative path using three steps' do
+      parse_xpath('A/B/C').should == s(
+        :path,
+        s(:axis, 'child', s(:test, nil, 'A')),
+        s(:axis, 'child', s(:test, nil, 'B')),
+        s(:axis, 'child', s(:test, nil, 'C')),
+      )
+    end
+
     it 'parses an expression using two paths' do
       parse_xpath('/A/B').should == s(
         :absolute_path,
         s(:axis, 'child', s(:test, nil, 'A')),
         s(:axis, 'child', s(:test, nil, 'B'))
+      )
+    end
+
+    it 'parses an expression using three paths' do
+      parse_xpath('/A/B/C').should == s(
+        :absolute_path,
+        s(:axis, 'child', s(:test, nil, 'A')),
+        s(:axis, 'child', s(:test, nil, 'B')),
+        s(:axis, 'child', s(:test, nil, 'C'))
       )
     end
 
