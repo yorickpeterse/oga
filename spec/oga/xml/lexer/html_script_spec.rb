351b5ac0040a804687f71173817b089925e14d0c
Yorick Peterse <yorickpeterse@gmail.com>
2015-03-02 16:20:06 +0100

    Added spec for lexing inline HTML script tags.
    Related issue: #70

---
+++ spec/oga/xml/lexer/html_script_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Oga::XML::Lexer do
+  describe 'HTML script elements' do
+    it 'treats all contents of a script tag as plain text' do
+      lex('<script>foo <bar</script>', :html => true).should == [
+        [:T_ELEM_START, nil, 1],
+        [:T_ELEM_NAME, 'script', 1],
+        [:T_TEXT, 'foo <bar', 1],
+        [:T_ELEM_END, nil, 1]
+      ]
+    end
+  end
+end
