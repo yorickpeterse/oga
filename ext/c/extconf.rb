4ca4c0b608ff6c3d395a044557a7171ed07389fb
Daniel Berger <djberg96@gmail.com>
2015-03-20 22:59:01 -0600

    Don't assume gcc.

--- ext/c/extconf.rb
+++ ext/c/extconf.rb
@@ -1,13 +1,16 @@
+require 'rbconfig'
 require 'mkmf'
 
 have_header('ruby.h')
 
-$CFLAGS << ' -Wextra -Wall -pedantic'
+if RbConfig::CONFIG['CC'] == 'gcc'
+  $CFLAGS << ' -Wextra -Wall -pedantic'
 
-if ENV['DEBUG']
-  $CFLAGS << ' -O0 -g'
-else
-  $CFLAGS << ' -O3'
+  if ENV['DEBUG']
+    $CFLAGS << ' -O0 -g'
+  else
+    $CFLAGS << ' -O3'
+  end
 end
 
 create_makefile('liboga')
