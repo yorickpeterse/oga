e6d2ba4e0efd208ab456bba7221f11e508151af8
Yorick Peterse <yorickpeterse@gmail.com>
2015-04-12 20:25:21 +0200

    Warm up caches in the XPath big XML benchmark

--- benchmark/xpath/evaluator/big_xml_average_bench.rb
+++ benchmark/xpath/evaluator/big_xml_average_bench.rb
@@ -2,6 +2,9 @@ require_relative '../../benchmark_helper'
 
 doc = Oga.parse_xml(big_xml_file)
 
+# Warm up any caches
+doc.xpath('descendant-or-self::location')
+
 measure_average do
   doc.xpath('descendant-or-self::location')
 end
