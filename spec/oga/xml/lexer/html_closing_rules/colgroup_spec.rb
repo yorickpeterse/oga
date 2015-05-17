2a1c5646f3ad5e3e8b64ec3f22bbf15f80e2fd23
Yorick Peterse <yorickpeterse@gmail.com>
2015-05-18 00:32:09 +0200

    Reworked HTML colgroup closing specs

--- spec/oga/xml/lexer/html_closing_rules/colgroup_spec.rb
+++ spec/oga/xml/lexer/html_closing_rules/colgroup_spec.rb
@@ -1,14 +1,32 @@
 require 'spec_helper'
 
 describe Oga::XML::Lexer do
-  describe 'HTML colgroup elements' do
-    it 'lexes an unclosed <colgroup> followed by another <colgroup>' do
+  describe 'using HTML <colgroup> elements' do
+    it 'lexes two unclosed <colgroup> elements as separate elements' do
       lex_html('<colgroup>foo<colgroup>bar').should == [
         [:T_ELEM_NAME, 'colgroup', 1],
         [:T_TEXT, 'foo', 1],
         [:T_ELEM_END, nil, 1],
         [:T_ELEM_NAME, 'colgroup', 1],
         [:T_TEXT, 'bar', 1],
+        [:T_ELEM_END, nil, 1]
+      ]
+    end
+
+    it 'lexes a <col> element following a <colgroup> as a child element' do
+      lex_html('<colgroup><col>').should == [
+        [:T_ELEM_NAME, 'colgroup', 1],
+        [:T_ELEM_NAME, 'col', 1],
+        [:T_ELEM_END, nil, 1],
+        [:T_ELEM_END, nil, 1]
+      ]
+    end
+
+    it 'lexes a <template> element following a <colgroup> as a child element' do
+      lex_html('<colgroup><template>').should == [
+        [:T_ELEM_NAME, 'colgroup', 1],
+        [:T_ELEM_NAME, 'template', 1],
+        [:T_ELEM_END, nil, 1],
         [:T_ELEM_END, nil, 1]
       ]
     end
