73855e6428d1a0ba06b413b25f98ddf181bbc391
Yorick Peterse <yorickpeterse@gmail.com>
2015-05-21 20:59:48 +0200

    Compare with Nokogiri in the HTML parser bench

--- benchmark/xml/parser/html_bench.rb
+++ benchmark/xml/parser/html_bench.rb
@@ -1,9 +1,17 @@
 require_relative '../../benchmark_helper'
 
+require 'nokogiri'
+
 html = read_html
 
 Benchmark.ips do |bench|
-  bench.report 'parse HTML' do
-    Oga::HTML::Parser.new(html).parse
+  bench.report 'Oga' do
+    Oga.parse_html(html)
   end
+
+  bench.report 'Nokogiri' do
+    Nokogiri::HTML(html)
+  end
+
+  bench.compare!
 end
