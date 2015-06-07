9f71af401a2b4d277f22aa8a7c72e38652772c1a
Iain Barnett <iainspeed@gmail.com>
2015-06-07 04:27:18 +0100

    Decoder didn't detect all ampersand codes. Fixed

--- spec/oga/entity_decoder_spec.rb
+++ spec/oga/entity_decoder_spec.rb
@@ -18,14 +18,33 @@ describe Oga::EntityDecoder do
   end
 
   describe 'decode' do
+    shared_examples "an HTML decoder" do
+      it "decodes it" do
+        described_class.decode(entity, true)
+          .should == Oga::HTML::Entities::DECODE_MAPPING[entity]
+      end
+    end
+
     it 'decodes XML entities' do
       described_class.decode('&lt;')
         .should == Oga::XML::Entities::DECODE_MAPPING['&lt;']
     end
 
-    it 'decodes HTML entities' do
-      described_class.decode('&copy;', true)
-        .should == Oga::HTML::Entities::DECODE_MAPPING['&copy;']
+    context 'HTML entities' do
+      context "Given a valid argument" do
+        context "of &copy;" do
+          let(:entity) { '&copy;' }
+          it_behaves_like "an HTML decoder"
+        end
+        context "Given an argument of &sup1;" do
+          let(:entity) { '&sup1;' }
+          it_behaves_like "an HTML decoder"
+        end
+        context "Given an argument of &frac15;" do
+          let(:entity) { '&frac15;' }
+          it_behaves_like "an HTML decoder"
+        end
+      end
     end
   end
 end
