7a4cb76d39a5d6a52538fb9e7226b7b3300b2aea
Yorick Peterse <yorickpeterse@gmail.com>
2015-08-19 23:05:25 +0200

    Cleaned up XPath last() specs a bit

--- spec/oga/xpath/compiler/calls/last_spec.rb
+++ spec/oga/xpath/compiler/calls/last_spec.rb
@@ -5,11 +5,18 @@ describe Oga::XPath::Compiler do
     before do
       @document = parse('<root><a>foo</a><a>bar</a></root>')
 
+      @a1 = @document.children[0].children[0]
       @a2 = @document.children[0].children[1]
     end
 
-    it 'returns a node set containing the last node' do
-      evaluate_xpath(@document, 'root/a[last()]').should == node_set(@a2)
+    describe 'in a predicate' do
+      it 'returns a NodeSet containing the last node' do
+        evaluate_xpath(@document, 'root/a[last()]').should == node_set(@a2)
+      end
+
+      it 'returns a NodeSet containing all but the last node' do
+        evaluate_xpath(@document, 'root/a[1 < last()]').should == node_set(@a1)
+      end
     end
   end
 end
